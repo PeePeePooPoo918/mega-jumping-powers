d108ae9a69893233ffd8f74da6809a585ccd5e44
PeePeePooPoo918 <Klarog72576@NBSS.NBED.NB.CA>
2024-10-09 11:20:35 -0300

    Initial files for MakeCode project

---
+++ main.ts
@@ -0,0 +1,18 @@
+player.onChat("jump", function (Trowel) {
+	
+})
+blocks.onBlockBroken(GRASS, function () {
+	
+})
+player.onChat("jump", function () {
+	
+    player.onChat("jump", function() {
+        
+    })
+})
+player.onChat("run", function () {
+	
+})
+blocks.onBlockPlaced(AIR, function () {
+	
+})
